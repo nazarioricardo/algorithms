9fd2a666d866d3c3701e280bbc9391dfbe6a82dd
Ricardo Nazario <nazarioricardo@gmail.com>
2023-10-20 09:33:35 -0400

    Two-pointer

--- reverse-vowels.ts
+++ reverse-vowels.ts
@@ -1,37 +1,58 @@
 const VOWELS = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+const isVowel = (c: string): boolean => VOWELS.includes(c);
+
+// function reverseVowels(s: string): string {
+//   const strArr = s.split("");
+//   let vowels: string[] = [];
+//   let indexes: number[] = [];
+
+//   for (const [index, c] of strArr.entries()) {
+//     if (isVowel(c)) {
+//       vowels.push(c);
+//       indexes.push(index);
+//     }
+//   }
+
+//   const reversedVowels = vowels.reverse();
+
+//   for (const [index, v] of reversedVowels.entries()) {
+//     const insertionIndex = indexes[index];
+//     strArr[insertionIndex] = v;
+//   }
+
+//   return strArr.join("");
+// }
 
 function reverseVowels(s: string): string {
-  const isVowel = (c: string): boolean => VOWELS.includes(c);
   const strArr = s.split("");
-  let vowels: string[] = [];
-  let indexes: number[] = [];
+  let left = 0;
+  let right = s.length - 1;
 
-  for (const [index, c] of strArr.entries()) {
-    if (isVowel(c)) {
-      vowels.push(c);
-      indexes.push(index);
+  while (left < right) {
+    let leftChar = strArr[left];
+    let rightChar = strArr[right];
+    const leftIsVowel = isVowel(leftChar);
+    const rightIsVowel = isVowel(rightChar);
+
+    const shouldSwap = leftIsVowel && rightIsVowel;
+    if (shouldSwap) {
+      strArr[right] = leftChar;
+      strArr[left] = rightChar;
+      left++;
+      right--;
+      continue;
     }
-  }
 
-  const reversedVowels = vowels.reverse();
+    if (!rightIsVowel) {
+      right--;
+    }
 
-  for (const [index, v] of reversedVowels.entries()) {
-    const insertionIndex = indexes[index];
-    strArr[insertionIndex] = v;
+    if (!leftIsVowel) {
+      left++;
+    }
   }
 
   return strArr.join("");
 }
-
-// function reverseVowels(s: string): string {
-//   let left = 0;
-//   let right = s.length - 1;
-
-//   while (left < right) {
-
-//   }
-
-//   return s;
-// }
 
 console.log(reverseVowels("hello"), reverseVowels("leetcode"));
