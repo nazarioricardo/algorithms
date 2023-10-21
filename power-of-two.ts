220955bea5799f8b3f2d8acb9bf756287d56391c
Ricardo Nazario <nazarioricardo@gmail.com>
2023-10-21 11:53:34 -0400

    With recursion

--- power-of-two.ts
+++ power-of-two.ts
@@ -1,6 +1,22 @@
 function isPowerOfTwo(n: number): boolean {
-  // Easy solution
-  return Number.isInteger(Math.log2(n));
+  /**
+   * Easy Solution:
+   */
+
+  // return Number.isInteger(Math.log2(n));
+
+  /**
+   * Recursion Solution
+   */
+  if (n === 1) {
+    return true;
+  }
+
+  if (n < 1) {
+    return false;
+  }
+
+  return isPowerOfTwo(n / 2);
 }
 
 console.log(isPowerOfTwo(2), isPowerOfTwo(3), isPowerOfTwo(4));
