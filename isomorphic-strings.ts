5afd3d3c7e5f5b3ea925ae224b20cd23c996e688
Ricardo Nazario <nazarioricardo@gmail.com>
2023-10-22 12:49:49 -0400

    With obj

--- isomorphic-strings.ts
+++ isomorphic-strings.ts
@@ -1,10 +1,14 @@
+type ObjectType = {
+  [key: string]: string;
+};
+
 function isIsomorphic(s: string, t: string): boolean {
   // check for corner case when two string are not equals by length
   if (s.length !== t.length) return false;
   // create map for s string mapping
-  const smap = new Map<string, string>();
+  const smap: ObjectType = {};
   // create map for t string mapping
-  const tmap = new Map<string, string>();
+  const tmap: ObjectType = {};
   // iterate over strings:
   for (let i = 0; i < s.length; i++) {
     // take char from string s
@@ -12,19 +16,19 @@ function isIsomorphic(s: string, t: string): boolean {
     // take char from string t
     const tchar = t[i];
     // check that we have mapping in smap and it is valid with current tchar
-    if (smap.has(schar) && smap.get(schar) !== tchar) {
+    if (schar in smap && smap[schar] !== tchar) {
       // if no return false
       return false;
     }
     // check that we have mapping in tmap and it is valid with current schar
-    if (tmap.has(tchar) && tmap.get(tchar) !== schar) {
+    if (tchar in tmap && tmap[tchar] !== schar) {
       // if no return false
       return false;
     }
     // create mapping for schar
-    smap.set(schar, tchar);
+    smap[schar] = tchar;
     // create mapping for tchar
-    tmap.set(tchar, schar);
+    tmap[tchar] = schar;
   }
   // return true if all mappings are valid
   return true;
